35a0e84c5aa4000cd4cfccf29d68b36137b1fd37
honghe <leopardsaga@gmail.com>
2021-12-02 11:04:24 +0800

    Fix bug of Bytes in Python 3.

--- dataset.py
+++ dataset.py
@@ -63,7 +63,7 @@ class lmdbDataset(Dataset):
                 img = self.transform(img)
 
             label_key = 'label-%09d' % index
-            label = str(txn.get(label_key.encode()))
+            label = txn.get(label_key.encode()).decode()
 
             if self.target_transform is not None:
                 label = self.target_transform(label)
